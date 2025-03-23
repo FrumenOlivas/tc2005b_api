24a80da88819587b2ef23c8705037bf7a841d698
Frumen Olivas <frumen@olivas.com>
2025-03-22 19:38:51 -0600

    rest api v1

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -7,14 +7,14 @@ export const login = async (req, res) => {
     values: [req.body.username],
   };
   const data = await sql.query(query);
-  console.log(data.rows);
-  console.log(data.rows.length);
+  // console.log(data.rows);
+  // console.log(data.rows.length);
   if (data.rows.length === 0) {
     res.json({ isLogin: false, user: {} });
     return;
   }
   if (req.body.password === data.rows[0].password) {
-    res.json({ isLogin: true, user: data.rows });
+    res.json({ isLogin: true, user: data.rows[0] });
     return;
   } else {
     res.json({ isLogin: false, user: {} });
